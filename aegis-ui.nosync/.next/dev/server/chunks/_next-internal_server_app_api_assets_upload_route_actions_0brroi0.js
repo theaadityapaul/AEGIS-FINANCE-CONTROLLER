2f68031cbfbb3d8a41d2b39e8e35b989fc9f74a4
Aaditya Paul <aadibpaul@gmail.com>
2026-09-04 20:33:17 +0530

    chore: remove node_modules and build artifacts from tracking

--- aegis-ui.nosync/.next/dev/server/chunks/_next-internal_server_app_api_assets_upload_route_actions_0brroi0.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/assets/upload/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__) => {
-"use strict";
-
-__turbopack_context__.s([]);
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_assets_upload_route_actions_0brroi0.js.map
